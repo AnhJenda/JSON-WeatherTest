4988a140044e8c1098094b60377483a24d211b7a
AnhJenda <93340795+AnhJenda@users.noreply.github.com>
2022-06-03 13:37:11 +0700

    update

--- weather.js
+++ weather.js
@@ -1,26 +1,35 @@
 'use strict';
 var weatherApp = angular.module('weatherApp', []);
 weatherApp.controller('WeatherCtrl', function ($scope, $http){
-    $http.get("https://api.openweathermap.org/data/2.5/weather?lat=21.027763&lon=105.834160&appid=a7a8a7ed30f023bb3a86e4d12a23ef3b&units=metric").success(function (data){
-        if (data){
-            $scope.current = data.main.temp;
-            $scope.temp_min = data.main.temp_min;
-            $scope.temp_max = data.main.temp_max;
-            $scope.wind_speed = data.main.wind_speed;
-            $scope.clouds = data.clouds ? data.clouds.all : undefined;
+        $scope.submit = function (){
+        const lat = document.getElementById("lat").value;
+        const lon = document.getElementById("lon").value;
 
-            var baseUrl =  'https://ssl.gstatic.com/onebox/weather/128/';
+            $http.get("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=a7a8a7ed30f023bb3a86e4d12a23ef3b&units=metric")
+                .success(function (data){
+                if (data){
+                    $scope.country = data.sys.country;
+                    $scope.name = data.name;
+                    $scope.current = data.main.temp;
+                    $scope.temp_min = data.main.temp_min;
+                    $scope.temp_max = data.main.temp_max;
+                    $scope.wind_speed = data.wind.speed;
+                    $scope.wind_deg = data.wind.deg;
+                    $scope.clouds = data.clouds ? data.clouds.all : undefined;
 
-            if ($scope.clouds < 20) {
-                $scope.img_url = baseUrl + 'sunny.png';
-            } else if ($scope.clouds < 90){
-                $scope.img_url = baseUrl + 'partly_cloudy.png';
-            }else {
-                $scope.img_url = baseUrl + 'cloudy.png';
-            }
-        }
-    })
-        .error(function (data, status){
-            console.log(data);
-        });
+                    var baseUrl =  'https://openweathermap.org/img/';
+
+                    if ($scope.clouds < 20) {
+                        $scope.img_url = baseUrl + 'wn/01d.png';
+                    } else if ($scope.clouds < 90){
+                        $scope.img_url = baseUrl + 'wn/10d@2x.png';
+                    }else {
+                        $scope.img_url = baseUrl + 'wn/04d@2x.png';
+                    }
+                }
+            })
+                .error(function (data, status){
+                    console.log(data);
+                });
+    }
 });
